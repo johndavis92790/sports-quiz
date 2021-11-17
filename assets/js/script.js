82ec4f9f6e6b99e45fdf86e20bd7611dc9118ca8
johndavis <johndavis92790@gmail.com>
2021-11-16 23:08:56 -0700

    major changes to many elements in the js, removed most elements in the html and recreated them in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,3 @@
-var body = document.main;
-
-var timerText = document.getElementById('timer');
-
-var startPageDiv = document.createElement("div");
-var startButton = document.createElement('button');
-
-var endPage = document.createElement("div");
-var highScores = document.createElement("div");
-
-var questionDiv = document.getElementById("question-div");
-var questionTitle = document.getElementById("question-title");
-var answerList = document.getElementById("answer-list");
-var answer1 = document.getElementById("answer-button-1");
-var answer2 = document.getElementById("answer-button-2");
-var answer3 = document.getElementById("answer-button-3");
-var answer4 = document.getElementById("answer-button-4");
-
-var questionCount = 0;
-var timeLeft = 10;
-
-var currentAnswer = 0;
-var currentScore = 0;
-var highScore = 0;
-
-
 var questions = [{
   question: 'Test question#1',
   answer: ['Q1Answer#1', 'Q1Answer#2', 'Q1Answer#3', 'Q1Answer#4'],
@@ -45,15 +19,17 @@ var questions = [{
   correctAnswer: 3
 }];
 
-function textAppendQuestionDiv(i){
-  questionTitle.textContent = (questions[i].question);
-  answer1.textContent = (questions[i].answer[0]);
-  answer2.textContent = (questions[i].answer[1]);
-  answer3.textContent = (questions[i].answer[2]);
-  answer4.textContent = (questions[i].answer[3]);
-}
+var questionCount = 1;
+var timeLeft = 59;
+
+var currentAnswer = 0;
+var currentScore = 0;
+var highScore = 0;
+
 
 function countdown(){
+  var timerText = document.getElementById('timer');
+
   // Use the setInterval method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
@@ -66,6 +42,7 @@ function countdown(){
       if (questionCount === 4){
         timerText.textContent = 'Times up!';
         clearInterval(timeInterval);
+        endQuiz();
       } else {
         timerText.textContent = 'Times up!';
         clearInterval(timeInterval);
@@ -75,89 +52,158 @@ function countdown(){
   }, 1000);
 }
 
-function highScores(){
-}
+function textAppendQuestionDiv(i){
+  var body = document.querySelector("main");
+  body.textContent = '';
 
-function startQuiz(){
-  countdown();
+  var questionDiv = document.createElement("div");
+  var questionTitle = document.createElement("h2");
+  var answerList = document.createElement("ol");
   
-  answer1.addEventListener("click", function() {
-    if (timeLeft > 0 && questionCount < 4) {
+  questionTitle.textContent = questions[i].question;
+  
+  for (var x = 0; x < questions[i].answer.length; x++) {
+    var answer = document.createElement("button");
+    answer.textContent = questions[i].answer[x];
+    answerList.appendChild(answer);
+    answer.addEventListener("click", function(event) {
+      if (timeLeft > 0 && questionCount < 4) {
+        currentAnswer = event.target;
+        console.log(currentAnswer);
         textAppendQuestionDiv(questionCount);
-        currentAnswer = 0;
         if (currentAnswer === questions[questionCount].correctAnswer){
           currentScore++;
         }
         questionCount++;
-    } else {
-      endQuiz();
-    }
-  });
-  answer2.addEventListener("click", function() {
-    if (timeLeft > 0 && questionCount < 4) {
-        textAppendQuestionDiv(questionCount);
-        currentAnswer = 1;
-        if (currentAnswer === questions[questionCount].correctAnswer){
-          currentScore++;
-        }
-        questionCount++;
-    }else {
-      endQuiz();
-    }
-  });
-  answer3.addEventListener("click", function() {
-    if (timeLeft > 0 && questionCount < 4) {
-        textAppendQuestionDiv(questionCount);
-        currentAnswer = 2;
-        if (currentAnswer === questions[questionCount].correctAnswer){
-          currentScore++;
-        }
-        questionCount++;
-    }else {
-      endQuiz();
-    }
-  });
-  answer4.addEventListener("click", function() {
-    if (timeLeft > 0 && questionCount < 4) {
-        textAppendQuestionDiv(questionCount);
-        currentAnswer = 3;
-        if (currentAnswer === questions[questionCount].correctAnswer){
-          currentScore++;
-        }
-        questionCount++;
-    }else {
-      endQuiz();
-    }
-  });
-}
+      } else {
+        console.log(currentAnswer);
+        endQuiz();
+      }
+      
+    });
 
-function startPage(){
-  var startPageTitle = document.createElement('h1');
-  startButton = document.createElement('button');
-  startPageTitle.textContent = "Initial Test Question";
-  startButton.textContent = "Start";
-  startPageDiv.appendChild(startPageTitle);
-  startPageDiv.appendChild(startButton);
-  questionDiv.replaceWith(startPageDiv);
+  }
 
-  startButton.addEventListener("click", startQuiz());
+  body.appendChild(questionDiv);
+  questionDiv.appendChild(questionTitle);
+  questionDiv.appendChild(answerList);
 }
 
 function endQuiz(){
+  timeLeft = 0;
+  var body = document.querySelector("main");
+  body.textContent = '';
+
+  var timerText = document.getElementById('timer');
+  timerText.textContent = 'Times up!';
+
+  var endDiv = document.createElement("div");
   var endPageTitle = document.createElement('h1');
   var endPageScore = document.createElement('h2');
-  if (currentScore >= highScore){
-    highScore = currentScore;
-    endPageTitle.textContent = "The quiz is over";
-    endPageTitle.textContent = ("You reached a new highscore of " + currentScore + "!");
-  } else {
-    endPageTitle.textContent = "The quiz is over";
-    endPageScore.textContent = ("Your Score was " + currentScore);
-  }
-  endPage.appendChild(endPageTitle);
-  endPage.appendChild(endPageScore);
-  questionDiv.replaceWith(endPage);
+  var endPageInitials = document.createElement('input');
+  var submitButton = document.createElement('button');
+
+  submitButton.textContent = "Click to submit score";
+
+  endPageTitle.textContent = "The quiz is over";
+  endPageScore.textContent = ("Your Score was " + currentScore);
+
+  // if (currentScore > highScore){
+  //   highScore = currentScore;
+  //   endPageTitle.textContent = "The quiz is over";
+  //   endPageScore.textContent = ("You reached a new highscore of " + currentScore + "!");
+  // } else if (currentScore = highScore){
+  //   endPageTitle.textContent = "The quiz is over";
+  //   endPageScore.textContent = ("You matched the highscore of " + currentScore + "!");
+  // } else {
+  //   endPageTitle.textContent = "The quiz is over";
+  //   endPageScore.textContent = ("Your Score was " + currentScore);
+  // }
+
+  body.appendChild(endDiv);
+  endDiv.appendChild(endPageTitle);
+  endDiv.appendChild(endPageScore);
+  endDiv.appendChild(endPageInitials);
+  endDiv.appendChild(submitButton);
+  
+  endPageInitials.addEventListener('input', function () {
+    endPageInitials.textContent = this.value;
+  });
+
+  submitButton.addEventListener("click", function() {
+    highScoreCheck();
+    highScores();
+
+  });
 
 }
 
-startPage(); 
+function highScoreCheck(){
+  
+}
+
+function startQuiz(){
+  var timerText = document.getElementById('timer');
+  timerText.textContent = 'Time: 1 Minute';
+  timeLeft = 59;
+  questionCount = 1;
+  currentAnswer = 0;
+  currentScore = 0;
+  countdown(timeLeft);
+  textAppendQuestionDiv(0);
+}
+
+function startPage(){
+  var body = document.querySelector("main");
+  body.textContent = '';
+
+  var timerText = document.getElementById('timer');
+  timerText.textContent = 'Time: 1 Minute';
+
+  var startDiv = document.createElement("div");
+  var startTitle = document.createElement('h1');
+  var startMessage = document.createElement('h2');
+  var startButton = document.createElement('button');
+
+  startTitle.textContent = "Welcome to the Sports Quiz";
+  startMessage.textContent = "Click below to start quiz";
+  startButton.textContent = "Start";
+
+  body.appendChild(startDiv);
+  startDiv.appendChild(startTitle);
+  startDiv.appendChild(startMessage);
+  startDiv.appendChild(startButton);
+  startButton.addEventListener("click", function() {
+    startQuiz();
+  });
+}
+
+function highScores(){
+  var body = document.querySelector("main");
+  body.textContent = '';
+
+  var currentScore = endQuiz(i);
+
+  console.log(currentScore);
+
+  var highScoreDiv = document.createElement("div");
+  var highScoreTitle = document.createElement('h1');
+  var highScoreList = document.createElement('ol');
+  var startButton = document.createElement('button');
+
+  highScoreTitle.textContent = "Highest Scores";
+  highScoreList.textContent = "Test";
+  startButton.textContent = "Start Over";
+
+  body.appendChild(highScoreDiv);
+  highScoreDiv.appendChild(highScoreTitle);
+  highScoreDiv.appendChild(highScoreList);
+  highScoreDiv.appendChild(startButton);
+  
+  startButton.addEventListener("click", function() {
+    startPage();
+  });
+
+}
+
+startPage();
